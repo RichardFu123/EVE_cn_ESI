434a894ae6002cdb4aeb6f68561cc171cec10408
Shawn <121857051@qq.com>
2018-12-04 09:37:06 +0800

    update : add a function in show_campaigns which can output the result as a txt file

--- show_campaigns.py
+++ show_campaigns.py
@@ -13,36 +13,48 @@ from Convert import utc2local
 from Player_info import alliance_info
 
 
-def try_print(st):
+content = ""
+
+
+def try_print(st, end='\n'):
+    global content
     try:
-        print(st)
+        print(st, end=end)
+        content += st+end
     except:
         print()
+        content += '\n'
         pass
 
 
+def make_file(file_name, content):
+    with open(file_name, "w") as f:
+        f.write(content)
+
+
 def show_campaigns(campaign):
-    print("="*10)
-    print("战斗编号: ", end='')
+    try_print("="*10)
+    try_print("战斗编号: ", end='')
     try_print(campaign["campaign_id"])
-    print("防守方: ", end='')
+    try_print("防守方: ", end='')
     try_print(alliance_info.alliance_info(campaign["defender_id"])["name"])
-    print("星域: ", end='')
+    try_print("星域: ", end='')
     try_print(region.get_region(constellation.get_constellation(campaign['constellation_id'])['region_id'])["name"])
-    print("星座: ", end='')
+    try_print("星座: ", end='')
     try_print(constellation.get_constellation(campaign["constellation_id"])['name'])
-    print("星系: ", end='')
+    try_print("星系: ", end='')
     try_print(system.get_system(campaign["solar_system_id"])["name"])
-    print("开始时间: ", end='')
+    try_print("开始时间: ", end='')
     try_print(utc2local.utc_str_2_local_str(campaign['start_time']))
-    print("作战目标: ", end='')
+    try_print("作战目标: ", end='')
     try_print(sovereignty_get_list.CampaignsEventType[campaign['event_type']].value)
-    print("目前进度: ", end='')
+    try_print("目前进度: ", end='')
     try_print(f"进攻方{campaign['attackers_score']*100}% VS 防守方{campaign['defender_score']*100}%")
-    print("="*10+"\n")
+    try_print("="*10+"\n")
 
 
 if __name__ == "__main__":
     campaigns_list = sovereignty_get_list.campaigns_list()
     for campaign in campaigns_list:
         show_campaigns(campaign)
+    make_file("show_campaigns.txt", content)
